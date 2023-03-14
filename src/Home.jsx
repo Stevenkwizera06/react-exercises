fb4024eb26eb18c891c36070599829d1e09c514e
Stevenkwizera06 <kwiste06@gmail.com>
2023-03-14 10:53:58 +0200

    worked on feedback about making a visible button to be clicked

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,11 @@ const Home = () => {
       <h1 className="text-7xl font-bold">React coding exercices</h1>
       <ul className="flex items-center justify-center h-full">
         <li>
-          <a className="text-4xl" href="/stateprops">State and props exercise</a>
+          <button className="border border-black px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            <a className="text-4xl" href="/stateprops">
+              State and props{" "}
+            </a>
+          </button>
         </li>
       </ul>
     </div>
