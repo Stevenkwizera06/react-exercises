487d6976d163703d9918c1580b54b9f32ab227d8
Stevenkwizera06 <kwiste06@gmail.com>
2023-03-14 11:24:16 +0200

    added a button in to the user click exercise in order to be visible

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <div className=" h-screen ">
       <h1 className="text-7xl font-bold text-center">React coding exercises</h1>
       <ul className=" text-center flex items-center justify-center h-full">
-        <li className="">
-          <a href="/stateprops">User click exercise</a>
+        <li>
+          <button className="border border-black px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            <Link className="text-4xl" to="/stateprops">
+              User click exercise
+            </Link>
+          </button>
         </li>
       </ul>
     </div>
