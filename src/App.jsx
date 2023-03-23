d0c04acf4a1cce4d70527dcc8a818bdda1543967
unknown <kwiste06@gmail.com>
2023-03-23 16:20:42 +0200

    Feat:Build a react form implemented the landing page Added a pop message for greeting Added the styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline bg-blue-500">React exercises</h1>;
-}
+import { useState } from 'react';
+
+const App = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    name === 'firstName' ? setFirstName(value) : setLastName(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello ${firstName} ${lastName}`);
+  };
+
+  return (
+    <div className='p-32 space-y-5 w-full bg-gray-50 m-12'>
+      <form className='flex flex-col w-48- gap-2 '>
+        <input
+          type='text'
+          name='firstName'
+          value={firstName}
+          placeholder='First Name'
+          onChange={handleChange}
+          className='border-2 p-1 rounded-md border-gray-300 placeholder:text-black'
+        />
+        <input
+          type='text'
+          name='lastName'
+          value={lastName}
+          placeholder='Last Name'
+          onChange={handleChange}
+          className='border-2 p-1 rounded-md border-gray-300 placeholder:text-black'
+        />
+      </form>
+      <button
+        className='bg-[#F0003C] p-3 rounded-md text-lg text-white w-full'
+        onClick={handleSubmit}
+      >
+        Greet me
+      </button>
+    </div>
+  );
+};
+
+export default App;
