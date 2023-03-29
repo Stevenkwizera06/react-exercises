6e93afd973324c4c28a1a65a884853c6b90d02c2
unknown <kwiste06@gmail.com>
2023-03-29 18:04:30 +0200

    corrected the suggested feedback i was given on descriptive names

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,18 +1,18 @@
-const CardComponent = ({lname, fname, employment, image}) => {
-    return ( 
-        <div className="w-[20rem] h-[25rem] group [perspective: 1000px]  p-5 bg-white rounded-xl shadow-sm  hover:shadow-xl hover:shadow-neutral-700 cursor-pointer shadow-black">
-          <div className="relative [transform-style: preserve-3d] group-hover:[transform:rotateY(180deg)] group-hover:[backface-visibility:hidden] transition-all duration-200">
-            <div className="absolute top-0 left-0 ">
-              <img alt="" src={image} className="object-cover"/>
-              <h2 className="text-2xl font-bold">{`${fname} ${lname}`}</h2>
-              <p className="font-thin text-lg">{employment.title}</p>
-            </div>
-            <div className="absolute top-10 [transform:rotateY(180deg)] duration-200 group-hover:[backface-visibility:visibile] [backface-visibility:hidden]">
-              <h3 className="text-3xl font-bold">More Information</h3>
-            </div>
+const CardComponent = ({firstName, lastName, employment, image}) => {
+  return ( 
+      <div className="w-[20rem] h-[25rem] group [perspective: 1000px]  p-5 bg-white rounded-xl shadow-sm  hover:shadow-xl hover:shadow-neutral-700 cursor-pointer shadow-black">
+        <div className="relative [transform-style: preserve-3d] group-hover:[transform:rotateY(180deg)] group-hover:[backface-visibility:hidden] transition-all duration-200">
+          <div className="absolute top-0 left-0 ">
+            <img alt="" src={image} className="object-cover"/>
+            <h2 className="text-2xl font-bold">{`${firstName} ${lastName}`}</h2>
+            <p className="font-thin text-lg">{employment.title}</p>
+          </div>
+          <div className="absolute top-10 [transform:rotateY(180deg)] duration-200 group-hover:[backface-visibility:visibile] [backface-visibility:hidden]">
+            <h3 className="text-3xl font-bold">More Information</h3>
           </div>
         </div>
-     );
+      </div>
+   );
 }
- 
+
 export default CardComponent;
